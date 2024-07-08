6f8c52a4c32a1248297caf665b2b0a579c8c0377
Atlas <46817089+AzeAstro@users.noreply.github.com>
2024-07-08 22:57:15 +0400

    Script updated
    Added installation and adding to bin directory for lnxrouter script

--- installer.py
+++ installer.py
@@ -4,5 +4,7 @@ import os
 
 print("Starting setup")
 print("Stage 1: Updating & installing packages")
-os.system("sudo apt update && sudo apt install bash procps iproute2 dnsmasq iptables hostapd iw haveged tmux -y")
+os.system("sudo apt update && sudo apt install bash procps iproute2 dnsmasq iptables hostapd iw haveged tmux wget -y")
 os.system("pip3 install psutil")
+print("Stage 2: Installing lnxrouter")
+os.system("wget https://raw.githubusercontent.com/garywill/linux-router/master/lnxrouter && chmod +x lnxrouter && cp lnxrouter /usr/bin")
